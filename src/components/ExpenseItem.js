a990319b9aae990c728a7d817052f299332a38af
suryansh2206 <suryansh804.sg@gmail.com>
2023-04-02 14:38:41 +0530

    Props_used

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,33 +1,14 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-    const expenseDate = new Date(2023, 4, 1);
-    const expenseTitle = 'Car Servicing';
-    const expenseAmount = 15000;
-    const locationOfExpenditure = 'Garage';
+function ExpenseItem(props) {
 
     return (
         <div className="expense-item">
-            <div>{expenseDate.toISOString()}</div>
+            <div>{props.date.toISOString()}</div>
             <div className="expense-item__description">
-                <h2>Food</h2>
-                <h6>Canteen</h6>
-                <div className='expense-item__price'>10rs</div>
-            </div>
-            <div className="expense-item__description">
-                <h2>Petrol</h2>
-                <h6>Petrol Pump</h6>
-                <div className='expense-item__price'>100rs</div>
-            </div>
-            <div className="expense-item__description">
-                <h2>Movies</h2>
-                <h6>PVR</h6>
-                <div className='expense-item__price'>200rs</div>
-            </div>
-            <div className="expense-item__description">
-                <h2>{expenseTitle}</h2>
-                <h6>{locationOfExpenditure}</h6>
-                <div className='expense-item__price'>{expenseAmount}</div>
+                <h2>{props.title}</h2>
+                <h6>{props.location}</h6>
+                <div className='expense-item__price'>{props.amount}</div>
             </div>
         </div>
     );
