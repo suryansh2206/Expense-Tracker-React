abc58130974646fdd4f7baed07d207e68834d247
suryansh2206 <suryansh804.sg@gmail.com>
2023-04-06 19:21:43 +0530

    adding expenses dynamically and adding key

--- src/App.js
+++ src/App.js
@@ -1,40 +1,47 @@
+import React, { useState } from 'react';
 import ExpenseItem from "./components/ExpenseItem";
 import Expenses from "./components/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const initial_expenses = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 400,
+    date: new Date(2020, 7, 14),
+    locationOfExpenditure: "Department Store",
+  },
+  {
+    id: "e2",
+    title: "New TV",
+    amount: 45000,
+    date: new Date(2021, 2, 12),
+    locationOfExpenditure: "Electronic Store",
+  },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 10000,
+    date: new Date(2021, 2, 28),
+    locationOfExpenditure: "Policy Office",
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 8500,
+    date: new Date(2021, 5, 12),
+    locationOfExpenditure: "Furniture Store",
+  },
+];
+
 const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 400,
-      date: new Date(2020, 7, 14),
-      locationOfExpenditure: "Department Store",
-    },
-    {
-      id: "e2",
-      title: "New TV",
-      amount: 45000,
-      date: new Date(2021, 2, 12),
-      locationOfExpenditure: "Electronic Store",
-    },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 10000,
-      date: new Date(2021, 2, 28),
-      locationOfExpenditure: "Policy Office",
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 8500,
-      date: new Date(2021, 5, 12),
-      locationOfExpenditure: "Furniture Store",
-    },
-  ];
+  const [expenses, setExpenses] = useState(initial_expenses)
+
+
   const addExpenseHandler = (expense) => {
-    console.log(expense)
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   }
 
   return (
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -16,30 +16,15 @@ const Expenses = (props) => {
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-        location={props.items[0].locationOfExpenditure}
-      />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-        location={props.items[1].locationOfExpenditure}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-        location={props.items[2].locationOfExpenditure}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-        location={props.items[3].locationOfExpenditure}
-      />
+      {props.items.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          date={expense.date}
+          amount={expense.amount}
+          location={expense.locationOfExpenditure}
+        />
+      ))}
     </Card>
   );
 };
