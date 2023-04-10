4ace9d87bef7c143d521103f276758129288bac4
suryansh2206 <suryansh804.sg@gmail.com>
2023-04-10 12:39:02 +0530

    final commit

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -42,7 +42,7 @@ const ExpenseForm = (props) => {
   return (
     <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
-        <div className="new_expense__control">
+        <div className="new-expense__control">
           <label>Title</label>
           <input
             type="text"
@@ -50,17 +50,17 @@ const ExpenseForm = (props) => {
             onChange={titleChangeHandler}
           />
         </div>
-        <div className="new_expense__control">
+        <div className="new-expense__control">
           <label>Amount</label>
           <input
             type="number"
-            min="0.01"
-            step="0.01"
+            min="1"
+            step="1"
             value={enteredAmount}
             onChange={amountChangeHandler}
           />
         </div>
-        <div className="new_expense__control">
+        <div className="new-expense__control">
           <label>Date</label>
           <input
             type="date"
@@ -70,7 +70,7 @@ const ExpenseForm = (props) => {
             onChange={dateChangeHandler}
           />
         </div>
-        <div className="new_expense__control">
+        <div className="new-expense__control">
           <label>Location</label>
           <input
             type="text"
@@ -79,7 +79,7 @@ const ExpenseForm = (props) => {
           />
         </div>
       </div>
-      <div className="new_expense__actions">
+      <div className="new-expense__actions">
         <button type="button" onClick={props.onCancel}>Cancel</button>
         <button type="submit">Add Expense</button>
       </div>
